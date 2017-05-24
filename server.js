410a3dbd7fb279780cd95fa220cbea33431f4924
Tom Langton <tom@pulpinfo.com>
2017-05-24 14:08:10 -0400

    interface from instructional

--- server.js
+++ server.js
@@ -90,7 +90,67 @@ app.get("/scrape", function(req, res) {
   res.send("Scrape Complete");
 });
 
-// Listen on port 3000
+// This will get the articles we scraped from the mongoDB
+app.get("/articles", function(req, res) {
+  // Grab every doc in the Articles array
+  Article.find({}, function(error, doc) {
+    // Log any errors
+    if (error) {
+      console.log(error);
+    } else {
+      // Or send the doc to the browser as a json object
+      res.json(doc);
+    }
+  });
+});
+
+// Grab an article by it's ObjectId
+app.get("/articles/:id", function(req, res) {
+  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  Article.findOne({ _id: req.params.id })
+    // ..and populate all of the notes associated with it
+    .populate("note")
+    // now, execute our query
+    .exec(function(error, doc) {
+      // Log any errors
+      if (error) {
+        console.log(error);
+      } else {
+        // Otherwise, send the doc to the browser as a json object
+        res.json(doc);
+      }
+    });
+});
+
+// Create a new note or replace an existing note
+app.post("/articles/:id", function(req, res) {
+  // Create a new note and pass the req.body to the entry
+  var newNote = new Note(req.body);
+
+  // And save the new note the db
+  newNote.save(function(error, doc) {
+    // Log any errors
+    if (error) {
+      console.log(error);
+    } else {
+      // Otherwise
+      // Use the article id to find and update it's note
+      Article.findOneAndUpdate({ _id: req.params.id }, { note: doc._id })
+        // Execute the above query
+        .exec(function(err, doc) {
+          // Log any errors
+          if (err) {
+            console.log(err);
+          } else {
+            // Or send the document to the browser
+            res.send(doc);
+          }
+        });
+    }
+  });
+});
+
+// Listen on port 3000 if not heroku
 app.listen(process.env.PORT || 3000, function() {
   console.log("App running on port 3000!");
 });
