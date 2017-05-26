2dca662a476caab5caeb596f61c78cc761cd6e8d
Tom Langton <tom@pulpinfo.com>
2017-05-26 15:27:51 -0400

    Friday almost - need note formatting

--- public/app.js
+++ public/app.js
@@ -19,7 +19,7 @@ $(document).on("click", ".new-note", function() {
     .done(function(data) {
       console.log(data);
       // The title of the articleo
-      $notes.append("<h2>" + data.title + "</h2>");
+      // $notes.append("<h2>" + data.title + "</h2>");
       // An input to enter a new title
       $notes.append("<input id='titleinput' name='title' >");
       // A textarea to add a new note body
